362422febcf7ce8126bce217836b6548e0f39e88
I-E-S-U <ian.samayoausher@gmail.com>
2023-05-05 11:25:29 +0200

    n

--- caesar.js
+++ caesar.js
@@ -20,3 +20,48 @@ function encryptCaesar() {
     } 
     document.getElementById('ptoc-ciphertext').innerHTML = o;   
 }
+
+function decryptCaesar() {
+    let p = document.getElementById('ptoc-plain').value;
+    let s = parseInt(document.getElementById('ptoc-shift').value);
+    let o = ''
+    for (let i = 0; i < p.length; i++) {
+        let j = p.charCodeAt(i);
+        if (j >= 65 && j <= 90) {
+            j = (j - 65 - s) % 26 + 65;
+            let c = String.fromCharCode(j);
+            o += c;
+        }
+        else if (j >= 97 && j <= 122) {
+            j = (j-97 - s) % 26 + 97;
+            let c = String.fromCharCode(j);
+            o += c;
+        }
+        else {
+            o += p[i]
+        }
+    } 
+    document.getElementById('ptoc-ciphertext').innerHTML = o;   
+}
+
+function Atbash() {
+    let p = document.getElementById('ctop-plain').value;
+    let o = ''
+    for (let i = 0; i < p.length; i++) {
+        let j = p.charCodeAt(i);
+        if (j >= 65 && j <= 90) {
+            j = (j - 90) * -1 + 65;
+            let c = String.fromCharCode(j);
+            o += c;
+        }
+        else if (j >= 97 && j <= 122) {
+            j = (j - 122) * -1 + 97;
+            let c = String.fromCharCode(j);
+            o += c;
+        }
+        else {
+            o += p[i]
+        }
+    } 
+    document.getElementById('ctop-ciphertext').innerHTML = o;   
+}
